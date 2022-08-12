255036defc804918ee095b3b427f0e8ba4e6d31f
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-08-12 16:16:19 +0300

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Node");
+const express = require('express');
+
+const PORT = 8080;
+
+const app = express();
+
+const serverFonk = () => {
+    console.log(`Server ${PORT} üzerinde yayında`);
+}
+
+app.listen(PORT, serverFonk)
