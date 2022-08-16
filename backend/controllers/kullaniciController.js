1ba62cb83ebfd070decbb1159cf891b2ef84d9ce
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-08-16 19:13:57 +0300

    Kullanıcı post eklendi

--- backend/controllers/kullaniciController.js
+++ backend/controllers/kullaniciController.js
@@ -1,7 +1,23 @@
+const asyncHandler = require('express-async-handler');
+const Kullanici = require('../models/kullaniciModel');
 
-const registerKullanici = (req,res) => {
+const registerKullanici = asyncHandler (async(req,res) => {
+
+    const {kullaniciAd, email, parola} = req.body;
+
+    if(!kullaniciAd || !email || !parola){        
+        res.status(400)
+        throw new Error('Lütfen Gerekli Alanları Doldurunuz')
+    }
+
+    const kullanici = await Kullanici.findOne({email})
+
+    if(kullanici){
+        res.status(400)
+        throw new Error('Bu email zaten kullanılıyor')
+    }
     res.json({mesaj:'Kullanıcı Register İşlemleri'})
-}
+})
 
 const loginKullanici = (req,res) => {
     res.json({mesaj:'Kullanıcı Login İşlemleri'})
