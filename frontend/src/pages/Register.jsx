9c888eb03e75156475dbaa1e98d4e55872134ecf
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-08-19 14:51:51 +0300

    register page okey

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,9 +3,82 @@ import { FaUser } from 'react-icons/fa';
 
 function Register() {
 
-  
+  const [formData, setFormData] = useState({
+    kullaniciAd: '',
+    email: '',
+    parola:'',
+    parolaKontrol:''
+  })
+
+  const { kullaniciAd, email, parola, parolaKontrol } = formData
+
+  const onChange = (e) => {
+    setFormData((onceki)=>({
+      ...onceki,
+      [e.target.name]:e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData);
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Üyelik Oluştur
+        </h1>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className='form-control' 
+              id="kullaniciAd"
+              name="kullaniciAd"
+              value={kullaniciAd}
+              placeholder="Kullanıcı Ad Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="email" 
+              className='form-control' 
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Email Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="password" 
+              className='form-control' 
+              id="parola"
+              name="parola"
+              value={parola}
+              placeholder="Parola Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="password" 
+              className='form-control' 
+              id="parolaKontrol"
+              name="parolaKontrol"
+              value={parolaKontrol}
+              placeholder="Parola Tekrarını Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>Üye Ol</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
