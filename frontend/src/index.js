1896a48cddca12a1a8a5afb5cded0e33907d44b7
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-08-19 18:14:20 +0300

    redux giriş ve paketler

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
+
+const topla = () => {
+  return{
+    type: 'TOPLA'
+  }
+}
+
+const cikar = () => {
+  return{
+    type:'CIKAR'
+  }
+}
+
+const hesapla = (state=0, action) => {
+  switch (action.type) {
+    case 'TOPLA':      
+      return state+1;
+    case 'CIKAR':      
+      return state-1;
+  }
+}
+
+let store = createStore(hesapla)
+
+store.subscribe(()=> console.log(store.getState()))
+
+store.dispatch(topla())
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaUserCheck } from 'react-icons/fa';
 
 function Login() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    parola:''
+  })
+
+  const { email, parola } = formData
+
+  const onChange = (e) => {
+    setFormData((onceki)=>({
+      ...onceki,
+      [e.target.name]:e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formData);
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUserCheck /> Giriş Yap
+        </h1>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input 
+              type="email" 
+              className='form-control' 
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Email Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="password" 
+              className='form-control' 
+              id="parola"
+              name="parola"
+              value={parola}
+              placeholder="Parola Giriniz"
+              onChange={onChange} />
+          </div>
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>Giriş</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
