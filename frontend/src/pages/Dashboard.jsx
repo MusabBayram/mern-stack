b1fe124a7eae5569b343154ad7ce2c3cb3e2d667
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-08-26 15:07:14 +0300

    get note function successfully

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,38 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import NotForm from '../components/NotForm';
+import { notlarGetir, reset } from '../features/data/dataSlice'
+import Spinner from '../components/Spinner'
 
 function Dashboard() {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const {kullanici} = useSelector(state => state.auth)
-
+  const {notlar, isYukleniyor, isHata, isBasari, mesaj} = useSelector(state => state.notlar)
+  
   useEffect(() => {
+    if(isHata){
+      console.log(mesaj);
+    }
     
     if(!kullanici){
       navigate('/login')
     }
-  }, [kullanici,navigate])
+    
+    dispatch(notlarGetir())
+
+    return () => {
+      dispatch(reset())
+    }
+
+  }, [kullanici,navigate,isHata, dispatch, mesaj])
+
+  if(isYukleniyor){
+    <Spinner />
+  }
+
   
 
   return (
