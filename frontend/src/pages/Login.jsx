6b018381a544fe87062f7166b6b7aab2adc0dd04
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-09-01 14:10:19 +0300

    login page changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,6 +6,17 @@ import { toast } from 'react-toastify';
 import { login, reset } from '../features/auth/authSlice';
 import Spinner from '../components/Spinner';
 
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+
 
 function Login() {
 
@@ -59,41 +70,60 @@ function Login() {
     <Spinner />
   }
 
+  // return (
+  //   <>
+  //     <section className='heading'>
+  //       <h1>
+  //         <FaUserCheck /> Giriş Yap
+  //       </h1>
+  //     </section>
+  //     <section className='form'>
+  //       <form onSubmit={onSubmit}>
+  //         <div className='form-group'>
+  //           <input 
+  //             type="email" 
+  //             className='form-control' 
+  //             id="email"
+  //             name="email"
+  //             value={email}
+  //             placeholder="Email Giriniz"
+  //             onChange={onChange} />
+  //         </div>
+  //         <div className='form-group'>
+  //           <input 
+  //             type="password" 
+  //             className='form-control' 
+  //             id="parola"
+  //             name="parola"
+  //             value={parola}
+  //             placeholder="Parola Giriniz"
+  //             onChange={onChange} />
+  //         </div>
+  //         <div className='form-group'>
+  //           <button type='submit' className='btn btn-block'>Giriş</button>
+  //         </div>
+  //       </form>
+  //     </section>
+  //   </>
+  // )
   return (
-    <>
-      <section className='heading'>
-        <h1>
-          <FaUserCheck /> Giriş Yap
-        </h1>
-      </section>
-      <section className='form'>
-        <form onSubmit={onSubmit}>
-          <div className='form-group'>
-            <input 
-              type="email" 
-              className='form-control' 
-              id="email"
-              name="email"
-              value={email}
-              placeholder="Email Giriniz"
-              onChange={onChange} />
-          </div>
-          <div className='form-group'>
-            <input 
-              type="password" 
-              className='form-control' 
-              id="parola"
-              name="parola"
-              value={parola}
-              placeholder="Parola Giriniz"
-              onChange={onChange} />
-          </div>
-          <div className='form-group'>
-            <button type='submit' className='btn btn-block'>Giriş</button>
-          </div>
-        </form>
-      </section>
-    </>
+    <Container maxWidth="md">
+      <Stack spacing={6}>
+        <Box>      
+          <Typography variant='h3' >
+            <AssignmentIcon color="info" sx={{fontSize:'45px'}} />  Login Sayfası
+          </Typography>
+        </Box>
+        <Divider />
+        <Box component="form" onSubmit={onSubmit} >
+          <TextField fullWidth label="Email Giriniz" type="email" name="email" value={email} onChange={onChange} margin="normal" />
+
+          <TextField fullWidth label="Parola Giriniz" type="password" name="parola" value={parola} onChange={onChange} margin="normal" />
+
+          <Button variant="contained" type="submit" color="success" fullWidth size="large" endIcon={<SendIcon />} sx={{marginTop:'25px'}}>Giriş Yap</Button>
+        </Box>
+    </Stack>
+  </Container>
   )
 }
 
