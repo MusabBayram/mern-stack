166a92bfe5842136c608d9ac1e1b31ef08836a2d
Musab Bahadır Bayram <musab.bayram@hotmail.com~>
2022-09-01 13:22:17 +0300

    mui material is added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,9 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import { useSelector, useDispatch} from 'react-redux'
 import { logout, reset } from '../features/auth/authSlice';
-
+import AppBar from '@mui/material/AppBar'
+import { Toolbar, Typography, Button } from '@mui/material'
+import { Login, Face, Home, Logout } from '@mui/icons-material'
 
 function Header() {
 
@@ -19,37 +21,69 @@ function Header() {
         navigate('/login')
     }
 
-  return (
-    <header className='header'>
-        <h2>Not Uygulaması</h2>
-        <div className='logo'>
+    const girisYap = () => {
+        navigate('/login')
+    }
 
-        </div>
-        <ul>
+    const uyeOl = () => {
+        navigate('/register')
+    }
+
+    const anasayfayaGit = () => {
+        navigate('/')
+    }
+
+//   return (
+//     <header className='header'>
+//         <h2>Not Uygulaması</h2>
+//         <div className='logo'>
+
+//         </div>
+//         <ul>
+//             {kullanici ? (
+//                 <>
+//                     <li>
+//                         <Link to="/"><FaPen />Not Oluştur</Link>
+//                     </li>      
+//                     <li>
+//                         <button className='btn' onClick={onLogout}>
+//                             <FaSignOutAlt /> Çıkış
+//                         </button>
+//                     </li>            
+//                 </>
+//             ) : (
+//                 <>
+//                     <li>
+//                         <Link to="/login"><FaSignInAlt />Giriş</Link>
+//                     </li>
+//                     <li>
+//                         <Link to="/register"><FaUser />Üye Ol</Link>
+//                     </li>  
+//                 </>
+//             )}            
+//         </ul>
+//     </header>
+//   )
+return (
+    <AppBar position='static' color='error' sx={{marginBottom:'50px'}} >
+        <Toolbar>
+            <Typography variant='h4' component="div" sx={{flexGrow:1}}>
+                Not Uygulaması
+            </Typography>
             {kullanici ? (
                 <>
-                    <li>
-                        <Link to="/"><FaPen />Not Oluştur</Link>
-                    </li>      
-                    <li>
-                        <button className='btn' onClick={onLogout}>
-                            <FaSignOutAlt /> Çıkış
-                        </button>
-                    </li>            
+                <Button sx={{marginRight: '10px'}} color='inherit' size='large' startIcon={<Home />} onClick={anasayfayaGit}>Anasayfa</Button>
+                <Button variant='outlined' color='inherit' size='small' startIcon={<Logout />} onClick={onLogout}>Çıkış</Button>
                 </>
-            ) : (
+            ): (
                 <>
-                    <li>
-                        <Link to="/login"><FaSignInAlt />Giriş</Link>
-                    </li>
-                    <li>
-                        <Link to="/register"><FaUser />Üye Ol</Link>
-                    </li>  
+                    <Button sx={{marginRight: '10px'}} color='inherit' size='large' startIcon={<Login />} onClick={girisYap}>Giriş</Button>
+                    <Button variant='outlined' color='inherit' size='small' startIcon={<Face />} onClick={uyeOl}>Üye Ol</Button>
                 </>
-            )}            
-        </ul>
-    </header>
-  )
+            )}
+        </Toolbar>
+    </AppBar>
+)
 }
 
 export default Header
